0ea0430267851a96242efd17ac3573fe8df07354
thegizmolab <hyuma.hoshi@gmail.com>
2025-11-27 14:13:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+// Carica il file JSON con la lista VHS
 fetch('data/vhs.json')
   .then(res => res.json())
   .then(data => {
+
+    // ORDINA ALFABETICAMENTE PER TITOLO
+    data.sort((a, b) => a.titolo.localeCompare(b.titolo));
+
     const grid = document.getElementById("grid");
 
-    // volendo, puoi ordinare per titolo:
-    // data.sort((a, b) => a.titolo.localeCompare(b.titolo));
-
+    // GENERA LA GRIGLIA
     data.forEach(item => {
       const card = creaCard(item);
       grid.appendChild(card);
@@ -15,18 +18,25 @@ fetch('data/vhs.json')
     console.error("Errore nel caricamento di vhs.json:", err);
   });
 
+
+// CREA UNA CARD SINGOLA
 function creaCard(item) {
+
+  // Div principale della card
   const div = document.createElement('div');
   div.className = "card";
+
+  // HTML della card (poster → front VHS)
   div.innerHTML = `
       <div class="inner">
           <div class="front">
-              <img src="${item.front}" alt="${item.titolo}">
+              <img src="${item.poster}" alt="${item.titolo} - Poster">
           </div>
           <div class="back">
-              <img src="${item.back}" alt="${item.titolo}">
+              <img src="${item.front}" alt="${item.titolo} - VHS Front">
           </div>
       </div>
   `;
+
   return div;
 }
